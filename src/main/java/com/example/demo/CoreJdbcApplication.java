b02dc92d61d867d5ef3dd0757bf37d6c1e32883b
vanshrajput01 <rajputvansh009@gmail.com>
2024-08-01 10:45:49 +0530

    intial code

---
+++ src/main/java/com/example/demo/CoreJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJdbcApplication.class, args);
+	}
+
+}
